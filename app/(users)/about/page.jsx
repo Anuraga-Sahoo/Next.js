ac850b46348df997f9d1254974da0309acfb1f85
Anuraga Sahoo <anuragliku.2020@gmail.com>
2025-10-05 12:47:25 +0530

    Seo optimazition

--- app/(users)/about/page.jsx
+++ app/(users)/about/page.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import myImage from '@/public/a.jpg'
 
+export const metadata = {
+    title: "About page",
+    description: "This is my company about page",
+    authors: [{name: "Anuraga Sahoo"}, {name: "get window", url: "youtube.com"}],
+    keywords: ["next.js", "react.js", "key words"]
+}
+
 const page = () => {
   return (
     <div className='w-full bg-red-300 flex items-center justify-center '>
--- app/admin/about/page.jsx
+++ app/admin/about/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+    title: "About page",
+    description: "This is my company about page"
+}
+
 const page = () => {
   return (
     <div>
